7c20bf71f4edb2a2a4eac3bef630400efbae0162
huynh <huynh@DESKTOP-NNLOED3>
2024-06-28 14:41:13 +0700

    setup project and test view hanghoa

--- ECommerceMVC/Program.cs
+++ ECommerceMVC/Program.cs
@@ -1,8 +1,14 @@
+using ECommerceMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<Hshop2023Context>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Hshop"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
