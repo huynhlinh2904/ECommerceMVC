744618c0ff1c53c068a8d249eefaad1a66f517de
huynh <huynh@DESKTOP-NNLOED3>
2024-07-18 09:25:17 +0700

    add MenuLoai in ViewComponents

---
+++ ECommerceMVC/ViewComponents/MenuLoai.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.ViewComponents
+{
+    public class MenuLoai : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+        public MenuLoai(Hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(loai => new
+            {
+                loai.MaLoai,
+                loai.TenLoai,
+                soluong = loai.HangHoas.Count
+            }).ToList();
+            return View();
+        }
+
+
+    }
+}
---
+++ ECommerceMVC/ViewComponents/ViewModels/MenuLoaiVM.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceMVC.ViewComponents.ViewModels
+{
+    public class MenuLoaiVM
+    {
+        public int maLoai { get; set; }
+        public string tenLoai { get; set; }
+        public int soLuong { get; set; }
+
+    }
+}
