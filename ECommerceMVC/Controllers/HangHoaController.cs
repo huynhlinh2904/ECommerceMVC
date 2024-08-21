d61b8da61bc9cdf2659d2889a7cd8170af4101d3
Huynh Linh <Huynh Linh@DESKTOP-DFAHC83>
2024-08-21 10:46:59 +0700

    search follow category

--- ECommerceMVC/Controllers/HangHoaController.cs
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMVC.Controllers
 {
     public class HangHoaController : Controller
     {
-        public IActionResult Index(int loai)
+        private readonly Hshop2023Context db;
+
+        public HangHoaController(Hshop2023Context context) 
         {
-            return View();
+            db = context;
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hangHoa = db.HangHoas.AsQueryable();
+
+            if (loai.HasValue)
+            {
+                hangHoa = hangHoa.Where(p => p.MaLoai == loai.Value);
+            }
+            var result = hangHoa.Select(p => new HangHoaVM
+            {
+                Id = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+
+
+            });
+            return View(result);
         }
     }
 }
