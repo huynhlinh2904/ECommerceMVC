191a2e585c3aec0791d480e97822bda10ef1a4c6
Huynh Linh <Huynh Linh@DESKTOP-DFAHC83>
2024-08-21 10:02:47 +0700

    change connect database

--- ECommerceMVC/Data/Hshop2023Context.cs
+++ ECommerceMVC/Data/Hshop2023Context.cs
@@ -53,7 +53,7 @@ public partial class Hshop2023Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-NNLOED3;Initial Catalog=Hshop2023;User ID=sa;Password=123456;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-DFAHC83\\HUYNHLINH;Initial Catalog=Hshop2023;User ID=sa;Password=123456;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
