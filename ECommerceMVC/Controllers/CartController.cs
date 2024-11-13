c63283a4c518735c66882f5f8e7d2b08874f6cf5
Huynh Linh <Huynh Linh@DESKTOP-DFAHC83>
2024-11-13 16:53:06 +0700

    add product to cart

---
+++ ECommerceMVC/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceMVC.Helpers;
+
+namespace ECommerceMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly Hshop2023Context db;
+
+        public CartController(Hshop2023Context context) 
+        {
+            db = context;
+        }
+        const string CART_KEY =  "MYCART";
+        public List<CartItem> Cart => HttpContext.Session.Get<List<CartItem>>(CART_KEY) ?? new List<CartItem>();
+        public IActionResult Index()
+        {
+            return View(Cart);
+        }
+        public IActionResult AddToCart(int id, int quantity = 1)
+        {
+            var gioHang = Cart;
+            var item = gioHang.SingleOrDefault(g => g.MaHH == id);
+            if (item == null)
+            {
+                var hangHoa = db.HangHoas.SingleOrDefault(h => h.MaHh == id);
+                if (hangHoa == null)
+                {
+                    TempData["Message"] = $"không tìm thấy hàng hóa có mã {id}";
+                    return Redirect("/404");
+                }
+                item = new CartItem
+                {
+                    MaHH = id,
+                    TenHH = hangHoa.TenHh,
+                    DonGia = hangHoa.DonGia ?? 0,
+                    Hinh = hangHoa.Hinh ?? string.Empty,
+                    SoLuong = quantity
+                };
+                gioHang.Add(item);
+            }
+            else
+            {
+                item.SoLuong += quantity;
+            }
+            HttpContext.Session.Set(CART_KEY, gioHang);
+            return RedirectToAction("index");
+        }
+        public IActionResult RemoveCart(int id) 
+        {
+            var gioHang = Cart;
+            var item = gioHang.SingleOrDefault(h => h.MaHH == id);
+            if(item != null)
+            {
+                gioHang.Remove(item);
+                HttpContext.Session.Set(CART_KEY , gioHang);
+            }
+            return RedirectToAction("index");
+        }
+    }
+}
--- ECommerceMVC/Program.cs
+++ ECommerceMVC/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddDbContext<Hshop2023Context>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("Hshop"));
 });
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +35,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
